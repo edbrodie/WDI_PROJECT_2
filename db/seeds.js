80f6b56e1948ad1f736f778290e10eb7bb0164af
edbrodie <edward.g.brode@gmail.com>
2017-10-25 12:24:48 +0100

    mvp complete

--- db/seeds.js
+++ db/seeds.js
@@ -30,21 +30,29 @@ User
     return Wine
       .create([{
         createdBy: users [0],
-        name: 'A tasty Rioja',
-        region: 'London',
-        pairings: 'bacon',
-        tastingNotes: 'really quite special... i need more of this now please',
-        buyLink: 'www.google.com',
-        image: 'https://i.pinimg.com/originals/1c/52/25/1c522569bc380d65fc08fc8824fdcbd5.png',
-        caption: 'vape reviews!'
+        name: 'Rioja Crianza 2013 Viña',
+        region: 'Northern Spain',
+        pairings: 'Rice dishes, Cheese, Pâté',
+        tastingNotes: 'black fruit and liquorice notes, with hints of cocoa and toast',
+        buyLink: 'https://www.majestic.co.uk/wines/rioja-crianza-14455',
+        image: 'https://i.pinimg.com/736x/35/e0/67/35e0678fda8eddc0f06d70e0af848cb2--wine-photography-wine-night.jpg'
       },{
         createdBy: users [0],
-        name: 'A weird shiraz',
-        region: 'manchester',
-        pairings: 'marmite',
-        tastingNotes: 'I wouldn\'t recommend this - really not tasty at all.',
-        buyLink: 'www.google.com',
-        image: 'https://www.strictlywine.co.uk/1817-thickbox_default/barossa-shiraz-reserve-berton.jpg'  }]);
+        name: 'Kangarilla Road Shiraz 2016',
+        region: 'Australia',
+        pairings: 'Roast Beef',
+        tastingNotes: 'Berry fruits and mixed spices',
+        buyLink: 'https://www.majestic.co.uk/wines/kangarilla-road-shiraz-19504',
+        image: 'http://photovino.com/wp-content/uploads/2016/07/DSC9446_500px.jpg'
+      },{
+        createdBy: users [1],
+        name: 'Parrilla Malbec 2015 Viñalba',
+        region: 'Argentina',
+        pairings: 'Pair with juicy steaks or sausages straight off the grill.',
+        tastingNotes: 'Damson, Blackberry and Plum flavours, accented with Oak Toast',
+        buyLink: 'https://www.majestic.co.uk/wines/parrilla-malbec-40144',
+        image: 'http://www.photopiaphotography.co.uk/img/photography/thumb/chase-cuvee-amphorae.jpg'
+      }]);
   })
   .then((wines) => console.log(`${wines.length} new and tasty wines created`))
   .catch((err) => console.log(err))
